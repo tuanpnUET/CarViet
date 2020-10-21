b13dee992cff8b7a35742c695d6b628f8c9ccbe7
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-10-21 21:07:49 +0700

    Create reg.php

---
+++ View/User/reg.php
@@ -0,0 +1,13 @@
+<h2><center>Đăng Ký</center></h2>
+<center><form name="frmthem" method="post" action="<?php WEBROOT . 'index.php/User/reg' ?>" enctype="multipart/form-data" style ="width: 30%">
+  <div class="form-group">
+    <label for="maloai">Tên tài khoản:</label>
+    <input type="text" class="form-control" name="taikhoan">
+  </div>
+  <div class="form-group">
+    <label for="tenloai">Mật khẩu:</label>
+    <input type="password" class="form-control" name="matkhau">
+  </div>
+   
+  <button type="submit" class="btn btn-default" style="background-color: green ">Đăng Ký Tài khoản</button>
+</form></center>
