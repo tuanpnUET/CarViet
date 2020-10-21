747821e74f286ad63d1c02b502a562c6c2e205bb
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-10-21 21:07:51 +0700

    Create detail.php

---
+++ View/News/detail.php
@@ -0,0 +1,19 @@
+
+	<center><h2 style="margin: auto" class="col-12 col-lg-12"> 
+		<?php echo $newss['tentintuc'] ?> 
+	</h2></center>
+
+<div class="row" id="Main" style="margin-top: 3%">
+	
+	<div id="left" class="col-lg-12 col-12" style="width: auto;">
+		<center><?php echo "<img src='".WEBROOT."Image/".$newss['anh']."' class='border' style='width:50%';>"?></center><br>
+	</div>
+		<div id="left2" class="col-lg-12 col-12">
+					<p style="text-align: justify;word-wrap: break-word">
+				<?php echo $newss['noidungtin']; ?>
+
+			</p>
+	</div>
+
+	
+</div>
