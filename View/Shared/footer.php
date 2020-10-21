3c38c36e4d1ab1d1a185a7439240df0db6be2821
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-10-21 21:07:33 +0700

    Create footer.php

---
+++ View/Shared/footer.php
@@ -0,0 +1,25 @@
+	<div class="row w-100" id="Footer" style="margin-left: 0px;" >
+			<div id="hotro" class="col-12 col-lg-4 xuong">
+				<ul>
+					<li>Hỗ Trợ Khách Hàng</li>
+					<li><a href="login.html">Hướng dẫn đặt hàng</a></li>
+					<li><a href="">Chính sách bảo hành</a></li>
+					<li><a href="">Câu hỏi thường gặp</a></li>
+					<li><a href="">Hỏi đáp</a></li>		
+				</ul>					
+			</div>
+			<div id="lienhe" class="col-12 col-lg-5 xuong">
+				<ul>
+					<li>Liên hệ</li>
+					<p>Địa Chỉ: Số 1 Hoàng Đạo Thúy,Hà Nội</p>
+					<p>Email: Tiemthuocminhtam@gmail.com</p>					
+				</ul>
+			</div>
+			<div id="hotline1" class="col-12 col-lg-3 xuong">
+				<ul>
+					<li><b>Hotline Hỗ Trợ </b></li>
+				  	<li><p class="hotline"><span class="glyphicon glyphicon-earphone"></span><b class="chu1">Tư vấn miễn phí</b><br><b class="chu1">0982282541</b></p></li>
+				  	<li><p class="hotline"><span class="glyphicon glyphicon-earphone"></span><b class="chu1">Tư vấn miễn phí</b><br><b class="chu1">0982282541</b></p></li>
+				</ul>	
+			</div>
+		</div>
