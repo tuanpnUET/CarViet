be1dd1965c62253087a7ac933bbbf9d573f573c8
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-10-21 21:31:38 +0700

    Create Filedispatcher ra admin.php

---
+++ test/Filedispatcher ra admin.php
@@ -0,0 +1,96 @@
+<?php
+class Request
+    {
+        public $url;
+
+        public function __construct()
+        {
+            $this->url = $_SERVER["REQUEST_URI"];
+        }
+    }
+class Router
+{
+
+    static public function parse($url, $request)
+    {
+        $url = trim($url);
+
+        if (strpos($url, 'CarViet/admin') !== false) {
+            $request->isAdmin = true;
+            if ($url == "/CarViet/admin/")
+            {
+                $request->controller = "Home";
+                $request->action = "index";
+                
+
+                $request->params = [];
+            }
+            else
+            {
+                $explode_url = explode('/', $url);
+                $explode_url = array_slice($explode_url, 2);
+                $request->controller = $explode_url[1];
+                $request->action = $explode_url[2];
+                $request->params = array_slice($explode_url, 3);
+                // echo '<pre>';
+                // print_r($request);
+            }
+
+
+        }
+        else
+        {
+
+            if ($url == "/CarViet/" or $url == "Carviet")
+            {
+                $request->controller = "Home";
+                $request->action = "index";
+                $request->isAdmin = false;
+
+                $request->params = [];
+            }
+            else
+            {
+                $explode_url = explode('/', $url);
+                $explode_url = array_slice($explode_url, 2);
+                $request->controller = $explode_url[1];
+                $request->action = $explode_url[2];
+                $request->params = array_slice($explode_url, 3);
+            }
+
+        }
+    }
+
+}
+class Dispatcher
+{
+
+    private $request;
+
+    public function dispatch()
+    {
+        $this->request = new Request();//$request->url=$SERVER["request_URI"]
+        Router::parse($this->request->url, $this->request);
+             $controller = $this->loadController();
+             $controller->set($this->request->params);
+
+        call_user_func_array([$controller, $this->request->action], $this->request->params);
+    }
+
+    public function loadController()
+    {
+
+        $name = $this->request->controller . "Controller";
+        if($this->request->isAdmin)
+                $file = ROOT . 'Controller/admin/' . $name . '.php';
+        else
+            $file = ROOT . 'Controller/' . $name . '.php';
+
+        // echo $file;
+        require($file);
+        $controller = new $name();
+        return $controller;
+    }
+
+}
+?>
